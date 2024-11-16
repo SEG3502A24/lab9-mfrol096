c99224fbf9c9df298780bb11073c589b123a18dc
mariyafrolova <mariyafro3@gmail.com>
2024-11-16 13:13:48 -0500

    commit

--- src/main/kotlin/seg3x02/tempconverterapi/TempConverterApiApplication.kt
+++ src/main/kotlin/seg3x02/tempconverterapi/TempConverterApiApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class TempConverterApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<TempConverterApiApplication>(*args)
+    runApplication<TempConverterApiApplication>(*args)
 }
